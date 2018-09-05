c029cd6131e9bfe2ac33c24e5cd4c85668a1645b
wanglove <wanglove@126.com>
2018-09-05 17:36:35 +0800

    增加项目启动文件和项目依赖包

--- webapp/init_db.py
+++ webapp/init_db.py
@@ -3,15 +3,15 @@ from . import app, db
 '''
 ***************数据库创建(by lei ge)*******************
 1.进入mysql创建数据库<db_name>,root用户登录mysql数据库执行以下语句:
-  CREATE DATABASE <db_name> CHARSET=UTF8
+  CREATE DATABASE geektest CHARSET=UTF8
 2.在config.py配置数据库连接
 3.执行在create_db.py
 '''
 
 # ***************创建数据库表*****************
-db.drop_all(app=app)
-db.create_all(app=app)
-db.session.commit()
+#db.drop_all(app=app)
+#db.create_all(app=app)
+#db.session.commit()
 
 
 
